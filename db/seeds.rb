a14c4f0a4b98732eb5afd68d4f7e2980b8b0974c
h4w5 <philip@h4w5.com>
2015-04-28 22:08:48 -0400

    remove scaffolded specs and add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+
+User.new(
+  email:                 "ann@ga.co",
+  name:                  "Ann",
+  password:              "ann",
+  password_confirmation: "ann"
+).save
+User.new(
+  email:                 "bill@ga.co",
+  name:                  "Bill",
+  password:              "bill",
+  password_confirmation: "bill"
+).save
+User.new(
+  email:                 "sue@ga.co",
+  name:                  "Sue",
+  password:              "sue",
+  password_confirmation: "sue"
+).save
