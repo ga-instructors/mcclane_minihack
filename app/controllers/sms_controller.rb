33fcf47ce6cfbc2e9856fc0b96556978443a236f
Nicholas Butterworth <nicholas.butterworth@gmail.com>
2015-05-08 19:57:08 -0400

    /joke working

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,5 +1,5 @@
 class SmsController < ApplicationController
-  before_action :authenticate
+  #before_action :authenticate
 
   def create
     sms          = TwilioIntegration.new
@@ -13,6 +13,14 @@ class SmsController < ApplicationController
   def show
     @phone_number = params[:phone_number]
     @message      = URI.decode params[:message]
+  end
+
+  def joke
+    sms          = TwilioIntegration.new
+    phone_number = "+1#{params["phone_number"].to_i}"
+    message = "A SQL query goes into a bar, walks up to two tables and asks, 'Can I join you?'"
+    logger.info sms.send(phone_number, message)
+    redirect_to "#{sms_path}?phone_number=#{phone_number}&message=#{URI.encode message}"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   resource  :session, only: [:new, :create, :destroy]
   root      "welcome#index"
+
+  post '/joke', to: 'sms#joke'
 end
