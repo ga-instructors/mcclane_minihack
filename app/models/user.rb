cc9e8eeef9e68144896db4a21e73a0b9899de399
Mark Smukler <smukler.mark@gmail.com>
2015-05-08 19:30:35 -0400

    added column to user table for phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, uniqueness: true
   validates :name,  presence: true
+  add_column :users, :phone, :string
 end
--- db/migrate/20150428191037_create_users.rb
+++ db/migrate/20150428191037_create_users.rb
@@ -8,5 +8,6 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps null: false
     end
     add_index :users, :email, unique: true
+    add_column :users, :phone, :string
   end
 end
