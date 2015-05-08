beac7c1dad20f1dec3c9a564e6af30479d3c32a4
Mark Smukler <smukler.mark@gmail.com>
2015-05-08 19:00:09 -0400

    added models for excuse and alarm and seed to excuses

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.delete_all
+Excuse.delete_all
 
 User.new(
   email:                 "ann@ga.co",
@@ -25,4 +26,24 @@ User.new(
   name:                  "Sue",
   password:              "sue",
   password_confirmation: "sue"
+).save
+
+
+Excuse.new(
+  excuse:                "You need to get to the office right now."
+).save
+Excuse.new(
+  excuse:                "Hey, your mom is looking for you. Can you call me ASAP?"
+).save
+Excuse.new(
+  excuse:                "I need you to call me right now - dad"
+).save
+Excuse.new(
+  excuse:                "CALL ME RIGHT NOW!"
+).save
+Excuse.new(
+  excuse:                "Your dog is in trouble! Where the hell are you?"
+).save
+Excuse.new(
+  excuse:                "This is a special message from the local police department. Your home was broken into. Please return home as soon as possible."
 ).save
