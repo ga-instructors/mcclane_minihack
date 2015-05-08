b6094dc69ebf7555daae18e9d0710e528b3da82f
Mark Smukler <smukler.mark@gmail.com>
2015-05-08 18:45:26 -0400

    added migrations

---
+++ db/migrate/20150508224450_create_alarms.rb
@@ -0,0 +1,6 @@
+class CreateAlarms < ActiveRecord::Migration
+  def change
+    create_table :alarms do |t|
+    end
+  end
+end
---
+++ db/migrate/20150508224515_create_excuses.rb
@@ -0,0 +1,6 @@
+class CreateExcuses < ActiveRecord::Migration
+  def change
+    create_table :excuses do |t|
+    end
+  end
+end
