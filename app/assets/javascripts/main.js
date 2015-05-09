f064556c0b118b23188a273759a3dbf77cf3354e
Qiao Lin <qiaoclin@gmail.com>
2015-05-08 21:03:36 -0400

    another route

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -15,7 +15,6 @@ $( document ).ready(function() {
 	        type: 'GET',
 	        url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=animal ",
 	      }).done(function(response){
-	      	console.log(response)
 	        $('body').css("background-image", "url(" + response.data.image_url + ")", "background-size", "cover")
 	      });
 
@@ -33,12 +32,15 @@ $( document ).ready(function() {
 		    }).done (function(data){
 		      console.log('it worked')
 		    });
-		 }else if (contentType === "pics"){
+		 }else if(contentType === "pics"){
+		 	
+		 	console.log("here in pics");
 		 	$.ajax({
 	        type: 'GET',
 	        url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=animal ",
 	      	}).done(function(response){
-
+	      		var image = response.data.image_url
+	      		console.log(image);
 			 	$.ajax({
 			    type: 'POST',
 			    url: '/pics',
